5bbca58cfdac5ece2195c37be0f69ceba5bda657
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-15 23:06:21 +0530

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
 
 function App() {
+  let [input,setInput] = useState('');
+  let [messages,setMessages] = useState(['hello','main thik','tum thik']);
+  console.log(messages);
+  let sendMessage = () => {
+    //logic to send the message
+    if(input != '')
+    {
+      setMessages([...messages,input]);
+      setInput('');
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <h1>CHAT🚀</h1>
+    <input value={input} onChange={e=>setInput(e.target.value)} type="text" />
+    <button onClick={sendMessage}>Send Message</button>
+    {
+      messages.map(message=>(
+        <p>{message}</p>
+      ))
+    }
     </div>
   );
 }
