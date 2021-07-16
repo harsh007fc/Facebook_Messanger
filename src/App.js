3679267f4249a3ef0f7801a830d825e9437ef6b2
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-17 00:39:41 +0530

    added upto usename

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import React,{useState} from 'react'
+import React, { useState,useEffect } from 'react'
 import './App.css';
+import { FormControl,Button,InputLabel,Input } from '@material-ui/core';
+import Message from './Components/Message';
 
 function App() {
-  let [input,setInput] = useState('');
-  let [messages,setMessages] = useState(['hello','main thik','tum thik']);
-  console.log(messages);
-  let sendMessage = () => {
+  let [input, setInput] = useState('');
+  let [messages, setMessages] = useState([{username:'main',text:'dog hai tu'},{username:'tum',text:'tum ho paas mere '},{username:'rana ji',text:'rana ji hai tu'}]);
+  let [username,setUsername] = useState('');
+
+  useEffect(()=>{
+    setUsername(prompt("Enter your name"))
+  },[])
+
+  let sendMessage = (e) => {
+    e.preventDefault();
     //logic to send the message
-    if(input != '')
-    {
-      setMessages([...messages,input]);
+    if (input != '') {
+      setMessages([...messages, {username:username,text:input}]);
       setInput('');
     }
   }
 
 
 
+
   return (
     <div className='App'>
-    <h1>CHAT🚀</h1>
-    <input value={input} onChange={e=>setInput(e.target.value)} type="text" />
-    <button onClick={sendMessage}>Send Message</button>
-    {
-      messages.map(message=>(
-        <p>{message}</p>
-      ))
-    }
+      <h1>CHAT🚀</h1>
+      <h2>Welcome {username}</h2>
+      <form action="">
+      <FormControl>
+        <InputLabel >Enter a message 💌</InputLabel>
+        <Input value={input} onChange={e => setInput(e.target.value)} type="text"/>
+        <Button disabled={!input}  variant="contained" color="primary" type='submit' onClick={sendMessage} varient="outlined" >Send Message</Button>
+      </FormControl>
+      </form>
+      {
+        messages.map(message => (
+          // <p>{message}</p>
+          <Message username={message.username} text={message.text}/>
+        ))
+      }
     </div>
   );
 }
---
+++ src/Components/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message(props) {
+    return (
+        <>
+          <h2>{props.username} :{props.text}</h2>
+           
+        </>
+    )
+}
+export default Message
