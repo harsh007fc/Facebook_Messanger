1af2d13b907ef529c01c03789506357e9a630e81
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-07-17 13:56:23 +0530

    added more css

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
-import { FormControl,Button,InputLabel,Input } from '@material-ui/core';
+import { FormControl, Button, InputLabel, Input } from '@material-ui/core';
 import Message from './Components/Message';
 import db from './Components/Firebase';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move'
 
 function App() {
   let [input, setInput] = useState('');
   let [messages, setMessages] = useState([]);
-  let [username,setUsername] = useState('');
+  let [username, setUsername] = useState('Unknown');
 
 
 
-  useEffect(()=>{
+  useEffect(() => {
     //run only once when app component loads
-    db.collection('messages').onSnapshot((snapshot)=>{
-      // console.log(snapshot.docs);
-      setMessages(snapshot.docs.map(doc=>doc.data()));
+    db.collection('messages').orderBy('timestamp','desc').onSnapshot((snapshot) => {
+      setMessages(snapshot.docs.map(doc => ({id : doc.id,message: doc.data()})));
     })
-  },[]);
-  useEffect(()=>{
+  }, []);
+  useEffect(() => {
     setUsername(prompt("Enter your name"))
-  },[])
+  }, []);
 
   let sendMessage = (e) => {
     e.preventDefault();
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     //logic to send the message
     if (input != '') {
-      setMessages([...messages, {username:username,message:input}]);
       setInput('');
     }
   }
 
-
-
-
   return (
     <div className='App'>
       <h1>CHAT🚀</h1>
+      <img src="https://en.facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" alt="facebook" />
       <h2>Welcome {username}</h2>
-      <form action="">
-      <FormControl>
-        <InputLabel >Enter a message 💌</InputLabel>
-        <Input value={input} onChange={e => setInput(e.target.value)} type="text"/>
-        <Button disabled={!input}  variant="contained" color="primary" type='submit' onClick={sendMessage} varient="outlined" >Send Message</Button>
-      </FormControl>
+      <form className='app__form'>
+        <FormControl>
+          <InputLabel >Enter a message 💌</InputLabel>
+          <Input value={input} onChange={e => setInput(e.target.value)} type="text" />
+          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage} varient="outlined" >Send Message</Button>
+        </FormControl>
       </form>
+      <FlipMove>
       {
-        messages.map(message => (
+        messages.map(({id , message}) => (
           // <p>{message}</p>
-          <Message username={username} message={message}/>
+          <Message key={id} username={username} message={message} />
         ))
       }
+      </FlipMove>
     </div>
   );
 }
--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React,{forwardRef} from 'react'
 import '../Css/Message.css'
 import {Card,CardContent,Typography} from '@material-ui/core';
-function Message({message,username}) {
+const Message = forwardRef(({message,username},ref) => {
   let isUser = username === message.username;
     return (
-        <div className={`message ${isUser && 'message__user'}`}>
+        <div ref={ref} className={`message ${isUser && 'message__user'}`}>
         <Card className = {isUser ? 'message__userCard' : 'message__guestCard' }>
           <CardContent>
             <Typography color='white' variant='h5' component='h2'>
@@ -15,5 +15,5 @@ function Message({message,username}) {
            
         </div>
     )
-}
+})
 export default Message
